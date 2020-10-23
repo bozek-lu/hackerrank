799b4eafa9b0feb4372fdb46219c559c0ecde84f
bozek-lu <bozek.lu@gmail.com>
2020-10-23 16:37:03 +0200

    more refactor

--- weighted uniform string.playground/Contents.swift
+++ weighted uniform string.playground/Contents.swift
@@ -2,21 +2,18 @@ import UIKit
 
 func weightedUniformStrings(s: String, queries: [Int]) -> [String] {
     let arrayOfWeights = s.map { Int($0.asciiValue ?? 0) - 96 }
-    
-    var contiguous: [[Int]] = []
-    
-    var partialResults: [Int] = [arrayOfWeights.first!]
-    contiguous.append(partialResults)
+    var contiguous: [Int] = []
+    contiguous.append(arrayOfWeights.first!)
+    var previous: Int?
     for i in arrayOfWeights {
-        if partialResults.last == i {
-            partialResults.append(i)
+        if previous == i {
+            contiguous.append(contiguous[contiguous.count - 1] + i)
         } else {
-            partialResults = [i]
+            contiguous.append(i)
         }
-        contiguous.append(partialResults)
+        previous = i
     }
-    let summed = contiguous.compactMap { $0.reduce(0, +) }
-    return queries.map { summed.contains($0) ? "Yes" : "No" }
+    return queries.map { contiguous.contains($0) ? "Yes" : "No" }
 }
 
-weightedUniformStrings(s: "abccddde", queries: [6,1,3,12,5,9,10])
+weightedUniformStrings(s:"abccddde", queries: [6,1,3,12,5,9,10])
